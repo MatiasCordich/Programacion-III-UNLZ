65a45101d3107570dda9927d92dd9955ccd4c1d8
MatiasCordich <m.scordich@gmail.com>
2024-03-29 19:45:48 -0300

    Agregar archivos de proyecto.

---
+++ Clase_01/WindowForm/Program.cs
@@ -0,0 +1,19 @@
+namespace WindowForm
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+
+            Form1 ventanita = new Form1();
+            Application.Run(ventanita);
+        }
+    }
+}
