a30cd2b99a2ea71b54163565effed22ed5f5323e
MatiasCordich <m.scordich@gmail.com>
2024-04-25 07:42:04 -0300

    Archivo create_table.sql agregado

---
+++ SQL/CLASE_05/create_table.sql
@@ -0,0 +1,17 @@
+-- CREATE TABLE
+
+/*
+Recordemos que nuestra tabla tenia los siguientes campos 
+ID de tipo INT
+BASE de tipo DECIMAL con 18 numeros en la parte entera y 2 para la parte decimal
+ALTURA de tipo DECIMAL con 18 numeros en la parte entera y 2 para la parte decimal
+
+Recordemos que NO SE PUEDE USAR TILES PARA LOS NOMBRES DE NUESTRAS TABLAS
+*/
+
+CREATE TABLE Rectangulos (
+ID INT,
+BASE DECIMAL(18,2),
+ALTURA DECIMAL(18,2)
+);
+
