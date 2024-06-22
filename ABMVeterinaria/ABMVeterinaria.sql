8586fdf6be1326552dd0aedfde751f95b56288e5
MatiasCordich <m.scordich@gmail.com>
2024-06-22 20:21:13 -0300

    ABM Veterinaria SQL modificado

--- ABMVeterinaria/ABMVeterinaria.sql
+++ ABMVeterinaria/ABMVeterinaria.sql
@@ -16,6 +16,11 @@ CREATE TABLE Clientes (
     Nombre VARCHAR(255) NOT NULL,
     DNI INT NOT NULL,
     Estado VARCHAR(10) NOT NULL DEFAULT 'Activo'
+)
+
+-- Modificacion de la Tabla Clientes para que DNI sea UNICO
+ALTER TABLE Clientes
+ADD CONSTRAINT UC_DNI UNIQUE (DNI);
 
 -- Aseguramos que Estado de Clientes solo acepte 'Activo' o 'Inactivo'
 ALTER TABLE Clientes
@@ -26,11 +31,11 @@ CREATE TABLE Animales (
     Nombre VARCHAR(100) NOT NULL,
     Peso DECIMAL(5,2) NOT NULL,
     Edad INT NOT NULL,
-	ClienteID INT, 
-	EspecieID INT,
-	FOREIGN KEY (ClienteID) REFERENCES Clientes(ClienteID),
-	FOREIGN KEY (EspecieID) REFERENCES Especies(EspecieID),
-	Estado VARCHAR(10) NOT NULL DEFAULT 'Vivo'
+    ClienteID INT, 
+    EspecieID INT NULL,  -- Permitir valores nulos
+    FOREIGN KEY (ClienteID) REFERENCES Clientes(ClienteID),
+    FOREIGN KEY (EspecieID) REFERENCES Especies(EspecieID),
+    Estado VARCHAR(10) NOT NULL DEFAULT 'Vivo'
 );
 
 -- Aseguramos que Estado de Animales solo acepte 'Vivo' o 'Difunto'
@@ -44,8 +49,6 @@ CREATE TABLE Especies (
 	PesoPromedio  DECIMAL(5,2) NOT NULL,
 );
 
-select * from Especies
-
 -- Usuario inicial 
 INSERT INTO Usuarios (NombreUsuario, Clave)
 VALUES ('pepe', '1234');
@@ -57,3 +60,8 @@ VALUES ('kali uchis', 38545676);
 -- Especie inicial
 INSERT INTO Especies(Nombre, EdadMadurez, PesoPromedio)
 VALUES ('canino', 4, 15);
+
+SELECT * FROM Usuarios
+SELECT * FROM Clientes
+SELECT * FROM Animales
+SELECT * FROM Especies
