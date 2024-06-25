8ac265bfb306e059e5b2e280633ac5a68eee66ab
MatiasCordich <m.scordich@gmail.com>
2024-06-25 00:26:31 -0300

    ABM Veterinaria: Alta Clientes finalizado

--- ABMVeterinaria/VeterinariaCleint/Formularios/Cliente/FormularioAltaCliente.Designer.vb
+++ ABMVeterinaria/VeterinariaCleint/Formularios/Cliente/FormularioAltaCliente.Designer.vb
@@ -22,16 +22,147 @@ Partial Class FormularioAltaCliente
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.P_altaCliente = New System.Windows.Forms.Panel()
+        Me.LBL_descripcionAltaCliente = New System.Windows.Forms.Label()
+        Me.PB_cargaAltaCliente = New System.Windows.Forms.ProgressBar()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Btn_altaCliente = New System.Windows.Forms.Button()
+        Me.Txt_DNIClienteAlta = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Txt_nombreClienteAlta = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.T_altaCliente = New System.Windows.Forms.Timer(Me.components)
+        Me.P_altaCliente.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'P_altaCliente
+        '
+        Me.P_altaCliente.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.P_altaCliente.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(67, Byte), Integer), CType(CType(67, Byte), Integer))
+        Me.P_altaCliente.Controls.Add(Me.LBL_descripcionAltaCliente)
+        Me.P_altaCliente.Controls.Add(Me.PB_cargaAltaCliente)
+        Me.P_altaCliente.Controls.Add(Me.GroupBox1)
+        Me.P_altaCliente.Location = New System.Drawing.Point(66, 45)
+        Me.P_altaCliente.Name = "P_altaCliente"
+        Me.P_altaCliente.Size = New System.Drawing.Size(668, 360)
+        Me.P_altaCliente.TabIndex = 2
+        '
+        'LBL_descripcionAltaCliente
+        '
+        Me.LBL_descripcionAltaCliente.AutoSize = True
+        Me.LBL_descripcionAltaCliente.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LBL_descripcionAltaCliente.ForeColor = System.Drawing.Color.FromArgb(CType(CType(149, Byte), Integer), CType(CType(210, Byte), Integer), CType(CType(179, Byte), Integer))
+        Me.LBL_descripcionAltaCliente.Location = New System.Drawing.Point(12, 283)
+        Me.LBL_descripcionAltaCliente.Name = "LBL_descripcionAltaCliente"
+        Me.LBL_descripcionAltaCliente.Size = New System.Drawing.Size(0, 19)
+        Me.LBL_descripcionAltaCliente.TabIndex = 2
+        Me.LBL_descripcionAltaCliente.Visible = False
+        '
+        'PB_cargaAltaCliente
+        '
+        Me.PB_cargaAltaCliente.Location = New System.Drawing.Point(16, 305)
+        Me.PB_cargaAltaCliente.Name = "PB_cargaAltaCliente"
+        Me.PB_cargaAltaCliente.Size = New System.Drawing.Size(634, 40)
+        Me.PB_cargaAltaCliente.TabIndex = 1
+        Me.PB_cargaAltaCliente.Visible = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Transparent
+        Me.GroupBox1.Controls.Add(Me.Btn_altaCliente)
+        Me.GroupBox1.Controls.Add(Me.Txt_DNIClienteAlta)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Txt_nombreClienteAlta)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(149, Byte), Integer), CType(CType(210, Byte), Integer), CType(CType(179, Byte), Integer))
+        Me.GroupBox1.Location = New System.Drawing.Point(16, 17)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(634, 220)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Alta de cliente"
+        '
+        'Btn_altaCliente
+        '
+        Me.Btn_altaCliente.BackColor = System.Drawing.Color.FromArgb(CType(CType(85, Byte), Integer), CType(CType(173, Byte), Integer), CType(CType(155, Byte), Integer))
+        Me.Btn_altaCliente.FlatAppearance.BorderSize = 0
+        Me.Btn_altaCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Btn_altaCliente.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(67, Byte), Integer), CType(CType(67, Byte), Integer))
+        Me.Btn_altaCliente.Location = New System.Drawing.Point(234, 153)
+        Me.Btn_altaCliente.Name = "Btn_altaCliente"
+        Me.Btn_altaCliente.Size = New System.Drawing.Size(138, 47)
+        Me.Btn_altaCliente.TabIndex = 4
+        Me.Btn_altaCliente.Text = "Registrar"
+        Me.Btn_altaCliente.UseVisualStyleBackColor = False
+        '
+        'Txt_DNIClienteAlta
+        '
+        Me.Txt_DNIClienteAlta.Location = New System.Drawing.Point(209, 104)
+        Me.Txt_DNIClienteAlta.Name = "Txt_DNIClienteAlta"
+        Me.Txt_DNIClienteAlta.Size = New System.Drawing.Size(348, 31)
+        Me.Txt_DNIClienteAlta.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(57, 112)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(42, 23)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "DNI"
+        '
+        'Txt_nombreClienteAlta
+        '
+        Me.Txt_nombreClienteAlta.Location = New System.Drawing.Point(209, 48)
+        Me.Txt_nombreClienteAlta.Name = "Txt_nombreClienteAlta"
+        Me.Txt_nombreClienteAlta.Size = New System.Drawing.Size(348, 31)
+        Me.Txt_nombreClienteAlta.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(57, 51)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 23)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre"
+        '
+        'T_altaCliente
+        '
         '
         'FormularioAltaCliente
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(85, Byte), Integer), CType(CType(173, Byte), Integer), CType(CType(155, Byte), Integer))
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.P_altaCliente)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximumSize = New System.Drawing.Size(816, 489)
+        Me.MinimumSize = New System.Drawing.Size(816, 489)
         Me.Name = "FormularioAltaCliente"
-        Me.Text = "FormularioAltaCliente"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Alta - Animal"
+        Me.P_altaCliente.ResumeLayout(False)
+        Me.P_altaCliente.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents P_altaCliente As Panel
+    Friend WithEvents LBL_descripcionAltaCliente As Label
+    Friend WithEvents PB_cargaAltaCliente As ProgressBar
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Btn_altaCliente As Button
+    Friend WithEvents Txt_DNIClienteAlta As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Txt_nombreClienteAlta As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents T_altaCliente As Timer
 End Class
--- ABMVeterinaria/VeterinariaCleint/Formularios/Cliente/FormularioAltaCliente.vb
+++ ABMVeterinaria/VeterinariaCleint/Formularios/Cliente/FormularioAltaCliente.vb
@@ -1,3 +1,95 @@
-﻿Public Class FormularioAltaCliente
+﻿Imports System.Windows.Forms.VisualStyles
+Imports VeterinariaService.DAO
+Imports VeterinariaService.Modelos
+
+Public Class FormularioAltaCliente
+
+#Region "Acciones"
+
+    '------------LIMPIEZA CAMPOS----------'
+    Private Sub LimpiezaCampos()
+        Txt_nombreClienteAlta.Text = ""
+        Txt_DNIClienteAlta.Text = ""
+    End Sub
+
+#End Region
+
+#Region "Eventos"
+
+    '----------Funcion ALTA CLIENTES----------------'
+    Private Sub Btn_altaCliente_Click(sender As Object, e As EventArgs) Handles Btn_altaCliente.Click
+        'Tomamos valores txtboxes'
+
+        Dim nombre = Txt_nombreClienteAlta.Text.ToLower
+        Dim dniS = Txt_DNIClienteAlta.Text
+
+        'Validacion - NO campos vacios'
+        If nombre = "" Or dniS = "" Then
+            MessageBox.Show("ERROR: No puede haber campos vacios")
+            Return
+        End If
+
+        'Validacion - DNI debe ser numerico'
+        Dim dni As Integer
+
+        If Not Integer.TryParse(dniS, dni) Then
+            MessageBox.Show("ERROR: Ingrese un valor numerico para DNI")
+            Return
+        End If
+
+        'Validacion - DNI deber ser positivo'
+        If dni < 0 Then
+            MessageBox.Show("ERROR: EL DNI debe ser POSITIVO")
+            Return
+        End If
+
+        'Inicio DAO'
+        Dim dao As New ClientesDAO
+
+        'Validadcion- NO nombre duplicado'
+        Dim listaClientes = dao.GetAll()
+
+        For Each cliente As Cliente In listaClientes
+            If cliente.DNI = dni Then
+                MessageBox.Show("ERROR: El DNI de cliente ya existe, intente otro")
+                Return
+            End If
+        Next
+
+        'Si paso la validacion prosigo con la inserccion'
+        Dim nuevoCliente As New Cliente(nombre, dni)
+
+        If dao.Insert(nuevoCliente) Then
+            PB_cargaAltaCliente.Visible = True
+            T_AltaCliente.Enabled = True
+            LBL_descripcionAltaCliente.Visible = True
+            LBL_descripcionAltaCliente.Text = "Cargando..."
+            MessageBox.Show("Cliente cargado correctamente")
+        End If
+    End Sub
+
+
+#End Region
+
+#Region "Timers"
+    Private Sub T_altaCliente_Tick(sender As Object, e As EventArgs) Handles T_altaCliente.Tick
+        'Evaluamos el valor de la barra de carga'
+        If PB_cargaAltaCliente.Value = PB_cargaAltaCliente.Maximum Then
+
+            'Si es igual a su MAXIMO'
+
+            'Limpiamos los Textbox y sacamos la barra de carga'
+            Me.LimpiezaCampos()
+            PB_cargaAltaCliente.Visible = False
+            LBL_descripcionAltaCliente.Visible = False
+        Else
+
+            'Caso contrario incrementamos el valor del a barra de carga'
+            Dim nuevoValor = PB_cargaAltaCliente.Value + 10
+            PB_cargaAltaCliente.Value = Math.Min(nuevoValor, PB_cargaAltaCliente.Maximum)
+        End If
+    End Sub
+
+#End Region
 
 End Class
