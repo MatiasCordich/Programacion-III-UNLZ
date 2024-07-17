09a40d3e8dbd879050d2b04b67982438c57de2bd
MatiasCordich <m.scordich@gmail.com>
2024-07-17 05:09:14 -0300

    Final C#

---
+++ Final_Programación/ABM_Remeras/Models/Remera.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABM_Remeras.Models
+{
+    internal class Remera
+    {
+        // ----- ATRIBUTOS ----- //
+        public string Codigo { get; set; }
+        public string Detalle { get; set; }
+        public double Precio { get; set; }
+        public string Talle { get; set; }
+
+    }
+}
